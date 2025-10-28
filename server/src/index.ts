7720d9c0083c10d059aaf46b00c1a48d3d7c42cc
George Chiriac <daddypumpkin@users.noreply.github.com>
2025-10-28 18:28:37 +0200

    Add copyright headers

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,8 +5,12 @@
  * All Rights Reserved.
  * NOTICE: All information contained herein is, and remains
  * the property of Adobe and its suppliers, if any. The intellectual
- * and technical concepts contained herein are proprietary to Adobe and its suppliers and are protected by all applicable intellectual property laws, including trade secret and copyright laws.
- *  Dissemination of this information or reproduction of this material is strictly forbidden unless prior written permission is obtained from Adobe.
+ * and technical concepts contained herein are proprietary to Adobe
+ * and its suppliers and are protected by all applicable intellectual
+ * property laws, including trade secret and copyright laws.
+ *  Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Adobe.
  */
 
 /// <reference types="@fastly/js-compute" />
